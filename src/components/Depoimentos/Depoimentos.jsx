fcf0aeb19afd294048e08bf02068116e6e3d21be
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2026-05-04 12:45:15 -0300

    up

--- src/components/Depoimentos/Depoimentos.jsx
+++ src/components/Depoimentos/Depoimentos.jsx
@@ -6,4 +6,7 @@ export const Depoimentos = [
     },{
         nome:"Claudinei Teles",profissao:"Gestão de RH", texto:"Você plantou muitas sementes de reflexão e mudança ao longo do caminho, e isso é transformador. Gratidão por tudo o que você representa na educação e na vida de quem cruza o seu caminho."
     },
+    {
+        nome:"Glauco Spina",profissao:"Diretor Financeiro", texto:"Participei do treinamento de Diversidade para Gestores, ministrado por Raquel Timoteo . O treinamento é muito técnico e a palestrante possui didática sensacional para abordar os temas de forma clara e objetiva para todos, preparando os colaboradores para lidar com a diversidade no ambiente de trabalho tornando-o mais inclusivo e respeitoso."
+    },
 ]
